a9599928864053ce28969df7e8d6d6de3b241fc4
EldiyarZh-cyber <74926819+EldiyarZh-cyber@users.noreply.github.com>
2021-04-26 06:47:28 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/style.css"
+          <title>Eldiyar</title>
+    </head>
+<body>
+    Hello Nurisalam Agai!!!!
+</body>
+</html>        
